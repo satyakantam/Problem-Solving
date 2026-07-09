26b5e083b6b59f2f75dfa906dd90abba445d54ed
satyakantam <satyamsahu.jh@gmail.com>
2026-07-09 21:15:02 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (99.22%) - LeetSync

---
+++ leetcode/812-rotate-string/rotate-string.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+
+        if(s.size()!=goal.size())   return false;
+
+        goal+=goal;
+        return goal.find(s) != string::npos;
+
+    }
+};
