6c05af5fcaf8bf9f36efc436bd7a54571d7b590f
satyakantam <satyamsahu.jh@gmail.com>
2026-08-05 22:54:46 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (11.20%) - LeetSync

---
+++ leetcode/1013-fibonacci-number/fibonacci-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int fib(int n) {
+        if(n==0)    return 0;
+        if(n==1)    return 1;
+        vector<int> arr(n+1);
+        arr[0] = 0;
+        arr[1] = 1;
+        int ans = 0;
+        for(int i = 2; i<n+1; i++)
+        {
+            arr[i] = arr[i-1] + arr[i-2];
+            
+        }
+        return arr[n];
+        
+    }
+};
